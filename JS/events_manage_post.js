f74752ea26b64addbb189cd0da6674a2429b9e5f
Juan Valdiviezo <valdiviezojuan7@gmail.com>
2024-05-20 00:18:36 -0500

    feat: Add modals in (login,manage_post)

--- JS/events_manage_post.js
+++ JS/events_manage_post.js
@@ -1,15 +1,56 @@
-const editarPost = document.getElementsByClassName("editar-post");
-const nuevoPost = document.getElementById("add-post");
+
+var editarPost;
+var nuevoPost;
+var deletePostBton;
 
 
-const postEdit = Array.from(editarPost);
 
-postEdit.forEach(post => {
-    post.addEventListener('click',function(){
+
+function deletePost(){
+    swal({
+        title: "¿Está seguro de eliminar la publicación?",
+        text: "Una vez eliminada, no se podrá recuperar!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+      })
+      .then((willDelete) => {
+        if (willDelete) {
+          swal("Su publicación ha sido eliminada", {
+            icon: "success",
+          });
+          //Remove the post from the database
+        } else {
+          swal("Su publicación está a salvo!");
+        }
+      });
+}
+
+function init(){
+    editarPost = document.getElementsByClassName("editar-post");
+    nuevoPost = document.getElementById("add-post");
+    deletePostBton = document.getElementsByClassName("btn-tabla delete-post");
+    const postEdit = Array.from(editarPost);
+    const deletePostItem = Array.from(deletePostBton);
+
+    
+    deletePostItem.forEach(item => {
+        item.addEventListener('click',deletePost);
+    });
+
+
+    postEdit.forEach(post => {
+        post.addEventListener('click',function(){
+            window.location.href = "new_product.html";
+        });
+    });
+
+    nuevoPost.addEventListener('click',function(){
         window.location.href = "new_product.html";
     });
-});
+}
 
-nuevoPost.addEventListener('click',function(){
-    window.location.href = "new_product.html";
-});
+document.addEventListener("DOMContentLoaded", init);
+
+
+
